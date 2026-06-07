ac2065759afd03d3249a57077a3c3abb70cfe26c
Jöran Karl <3951388+JoeKar@users.noreply.github.com>
2026-06-07 12:30:07 +0200

    Switch `tcell` to upstream `v3` and overwrite with our fork from `micro-editor`
    With the fork we can still use raw escape sequences until something similar is
    present upstream.
    
    Adapt to the style and clipboard interface changes:
    - replace Decompose() with dedicated getters
    - replace attributes with dedicated getters
    
    We have to update our required go toolchain version to 1.25 due to the fact
    that tcell updated its toolchain dependency to 1.25.

--- internal/config/colorscheme_test.go
+++ internal/config/colorscheme_test.go
@@ -3,14 +3,15 @@ package config
 import (
 	"testing"
 
-	"github.com/micro-editor/tcell/v2"
+	"github.com/gdamore/tcell/v3"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSimpleStringToStyle(t *testing.T) {
 	s := StringToStyle("lightblue,magenta")
 
-	fg, bg, _ := s.Decompose()
+	fg := s.GetForeground()
+	bg := s.GetBackground()
 
 	assert.Equal(t, tcell.ColorBlue, fg)
 	assert.Equal(t, tcell.ColorPurple, bg)
@@ -19,29 +20,32 @@ func TestSimpleStringToStyle(t *testing.T) {
 func TestAttributeStringToStyle(t *testing.T) {
 	s := StringToStyle("bold cyan,brightcyan")
 
-	fg, bg, attr := s.Decompose()
+	fg := s.GetForeground()
+	bg := s.GetBackground()
 
 	assert.Equal(t, tcell.ColorTeal, fg)
 	assert.Equal(t, tcell.ColorAqua, bg)
-	assert.NotEqual(t, 0, attr&tcell.AttrBold)
+	assert.True(t, s.HasBold())
 }
 
 func TestMultiAttributesStringToStyle(t *testing.T) {
 	s := StringToStyle("bold italic underline cyan,brightcyan")
 
-	fg, bg, attr := s.Decompose()
+	fg := s.GetForeground()
+	bg := s.GetBackground()
 
 	assert.Equal(t, tcell.ColorTeal, fg)
 	assert.Equal(t, tcell.ColorAqua, bg)
-	assert.NotEqual(t, 0, attr&tcell.AttrBold)
-	assert.NotEqual(t, 0, attr&tcell.AttrItalic)
-	assert.NotEqual(t, 0, attr&tcell.AttrUnderline)
+	assert.True(t, s.HasBold())
+	assert.True(t, s.HasItalic())
+	assert.True(t, s.HasUnderline())
 }
 
 func TestColor256StringToStyle(t *testing.T) {
 	s := StringToStyle("128,60")
 
-	fg, bg, _ := s.Decompose()
+	fg := s.GetForeground()
+	bg := s.GetBackground()
 
 	assert.Equal(t, tcell.Color128, fg)
 	assert.Equal(t, tcell.Color60, bg)
@@ -50,7 +54,8 @@ func TestColor256StringToStyle(t *testing.T) {
 func TestColorHexStringToStyle(t *testing.T) {
 	s := StringToStyle("#deadbe,#ef1234")
 
-	fg, bg, _ := s.Decompose()
+	fg := s.GetForeground()
+	bg := s.GetBackground()
 
 	assert.Equal(t, tcell.NewRGBColor(222, 173, 190), fg)
 	assert.Equal(t, tcell.NewRGBColor(239, 18, 52), bg)
@@ -68,7 +73,9 @@ color-link constant.string.char "#BDE6AD,#282828"`
 	c, err := ParseColorscheme("testColorscheme", testColorscheme, nil)
 	assert.Nil(t, err)
 
-	fg, bg, _ := c["comment"].Decompose()
+	fg := c["comment"].GetForeground()
+	bg := c["comment"].GetBackground()
+
 	assert.Equal(t, tcell.NewRGBColor(117, 113, 94), fg)
 	assert.Equal(t, tcell.NewRGBColor(40, 40, 40), bg)
 }
